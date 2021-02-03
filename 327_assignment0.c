5c7bb60bada5de2eaad93cdd755f1caa02b40181
kmytien <kmytien@iastate.edu>
2021-02-03 16:17:53 -0600

    Fixing errors in update()

--- 327_assignment0.c
+++ 327_assignment0.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #define WORLD_LENGTH 23
 #define WORLD_WIDTH 23
 
 void initTable();
 void printTable();
-void update(int row, int col, int height);
-void run(int row, int col, int height);
+void update(int row, int col);
+void run(int row, int col, int fps);
 
 int table[WORLD_LENGTH][WORLD_WIDTH];
 
@@ -17,9 +18,7 @@ int table[WORLD_LENGTH][WORLD_WIDTH];
 //main function
 //argc is number of parameters, argv[] is parameter array
 int main(int argc, char* argv[]) {
-    // default fps is
-    int fps = 30,
-    int j = 1, i;
+    int fps = 30, j = 1, i;
     int row, col, height;
 
     //we need to use atoi
@@ -40,18 +39,19 @@ int main(int argc, char* argv[]) {
 
     // accept and pass on an unlimited number of parameters y x h
     //it is i+=3 because we're updating the net three numbers below
-    for (i = j; i <= argc; i+=3) { //i+=3??????????
+    for (i = j; i <= argc; i += 3) { //i+=3??????????
 
         row = atoi(argv[i]);
         col = atoi(argv[i + 1]);
         height = atoi(argv[i + 2]);
-
-        update(row, col, height);
     }
 
 
-    // run simulation forever
-    run(11, 11, argv[j], fps);
+    run(11, 11, fps);
+
+
+    //run simulation forever
+    //run(11, 11, 1);
 
 }
 
@@ -78,60 +78,115 @@ void printTable() {
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 //update the grid/run the game
-void update(int row, int col, int height) {
-    int i = row;
-    int j = col;
-    table[row][col] = height;
+void update(int row, int col) {
+    int i = row, j = col;
+    table[row][col]++; //= height;
 
     // BASE CASE: if -1 or a sink, stays at height = -1
-    if(table[i][j] < 0)
+    if (table[i][j] < 0)
         table[i][j] = -1;
 
     // BASE CASE: if < 8 then add 1 to the heap and move on
-    if(num < 8)
-        table[i][j]++;
+    if (table[row][col] < 8) {
+       return;
+    }
 
     // else if its >= 8 then topple heap by
-    else if (num >= 8) {
+    else if (table[row][col] > 8) {
         table[i][j] = 1;
 
-        if(i + 1 < 23)
-            update(i + 1, j, table[i+1][j]++); //bottom box
+        if(i + 1 < 23) {
+            //update(i + 1, j); //bottom box
+            table[i + 1][j]++;
 
-        if(i - 1 >= 0)
-            update(i-1, j, table[i-1][j]++); //top box
+            if (table[i + 1][j] > 8)
+                update(i + 1, j);
 
-        if(j + 1 < 23)
-            update(i, j + 1, table[i][j++]++); //right box
+            //i++;
+            //update(i, j);
+        }
+        if(i - 1 >= 0) {
+            //update(i - 1, j); //top box
+            table[i - 1][j]++;
 
-        if(j - 1 >= 0)
-            update(i, j - 1, table[i][j-1]++); //left box
+            if (table[i - 1][j] > 8)
+                update(i - 1, j);
+            //i--;
+            //update(i, j);
+        }
+        if(j + 1 < 23) {
+            //update(i, j + 1); //right box
+            table[i][j + 1]++;
 
-        if(i - 1 >= 0 && j - 1 >= 0)
-            update(i - 1, j - 1, table[i-1][j-1]++); //top left
+            if (table[i][j + 1] > 8)
+                update(i, j + 1);
+            //j++;
+            //update(i, j);
+        }
+        if(j - 1 >= 0) {
+            //update(i, j - 1); //left box
+            table[i][j - 1]++;
 
-        if(i - 1 >= 0 && j + 1 < 23)
-            update(i - 1, j + 1, table[i - 1][j+1]++); //top right
+            if (table[i][j - 1] > 8)
+                update(i, j - 1);
+            //j--;
+            //update(i, j);
+        }
+        if(i - 1 >= 0 && j - 1 >= 0) {
+            //update(i - 1, j - 1); //top left
+            table[i - 1][j - 1]++;
 
-        if(i + 1 < 23 && j - 1 >= 0)
-            update(i + 1, j - 1, table[i+1][j-1]++); //bottom left
+            if (table[i - 1][j - 1] > 8)
+                update(i - 1, j - 1);
+            //i--;
+            //j--;
+            //update(i, j);
+        }
+        if(i - 1 >= 0 && j + 1 < 23) {
+            //update(i - 1, j + 1); //top right
+            table[i - 1][j + 1]++;
 
-        if(i + 1 < 23 && j + 1 < 23)
-            update(i + 1, j + 1, table[i + 1][j + 1]++); //bottom right
+            if (table[i - 1][j + 1] > 8)
+                update(i - 1, j + 1);
+            //i--;
+            //j++;
+            //update(i, j);
+        }
+        if(i + 1 < 23 && j - 1 >= 0) {
+            //update(i + 1, j - 1); //bottom left
+            table[i + 1][j - 1]++;
+
+            if (table[i + 1][j - 1] > 8)
+                update(i + 1, j - 1);
+            //i++;
+            //j--;
+            //update(i, j);
+        }
+        if(i + 1 < 23 && j + 1 < 23) {
+            //update(i + 1, j + 1); //bottom right
+            table[i + 1][j + 1]++;
+
+            if (table[i + 1][j + 1] > 8)
+                update(i + 1, j + 1);
+            //i++;
+            //j++;
+            //update(i, j);
+        }
+
     }
 }
 
 // infinite loop of update
 // row col and heigh are 11 11 and 1 respectively
-void run(int row, int col, int height, int fps) {
+void run(int row, int col, int fps) {
     while(true) {
-        update(row, col, height);
         printTable();
+        update(row, col);
 
-        // usleep pauses program for a sec
         usleep(1000000 / fps);
     }
 }
